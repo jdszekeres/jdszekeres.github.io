56e6e68a60103c5a30200dc1c38065b760725718
jdszekeres <69050063+jdszekeres@users.noreply.github.com>
2022-04-23 10:06:17 -0700

    Create wordguess.word.js

---
+++ wordguess.word.js
@@ -0,0 +1,33 @@
+Date.prototype.addDays=function(d) {
+    return new Date(this.getTime() + d*86400000); // milliseconds
+ };
+async function word_for_today() {
+    var start = new Date("04/21/22");
+    var today = new Date();
+    var Difference_In_Time = today.getTime() - start.getTime();
+    // To calculate the no. of days between two dates
+    var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+    var list = ["word", "moon", "create", "updated", "fortunate", "secondly", "cringe", "undid", "arch", "universe", "plum", "inspect", "upset", "transcontinental", "uncontrollable", "asynchronous", "facilitate", "uproar"];
+    console.log("words until "+start.addDays(list.length));
+    return list[Math.floor(Difference_In_Days)]
+}
+x = "";
+
+
+
+async function related(solution_word) {
+    trigger_req = await fetch("https://api.datamuse.com/words?rel_trg="+solution_word);
+    trigger = await trigger_req.json();
+    sim_req = await fetch("https://api.datamuse.com/words?ml="+solution_word);
+    sim = await sim_req.json();
+    rythme_req = await fetch("https://api.datamuse.com/words?rel_rhy="+solution_word);
+    rythme = await rythme_req.json();
+    return [trigger, sim, rythme];
+}
+
+
+async function get_word() {
+    p = await word_for_today();
+    x = await related(p);
+    return [p, x]; 
+}
