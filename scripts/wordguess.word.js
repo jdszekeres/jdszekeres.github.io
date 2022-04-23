46bddd6bccb599fb01f7ce07120bcfebebf99e09
jdszekeres <69050063+jdszekeres@users.noreply.github.com>
2022-04-23 14:29:21 -0700

    make sure local copy has same word list
    word list is synchronous

--- scripts/wordguess.word.js
+++ scripts/wordguess.word.js
@@ -7,13 +7,17 @@ async function word_for_today() {
     var Difference_In_Time = today.getTime() - start.getTime();
     // To calculate the no. of days between two dates
     var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-    var list = ["word", "moon", "create", "updated", "fortunate", "secondly", "cringe", "undid", "arch", "universe", "plum", "inspect", "upset", "transcontinental", "uncontrollable", "asynchronous", "facilitate", "uproar"];
+    var list = await get_list();
     console.log("words until "+start.addDays(list.length));
     return list[Math.floor(Difference_In_Days)]
 }
 x = "";
 
-
+async function get_list() {
+    var res = await fetch("https://raw.githubusercontent.com/jdszekeres/jdszekeres.github.io/main/resources/wordguess.words.json");
+    var json = await res.json();
+    return json;
+}
 
 async function related(solution_word) {
     trigger_req = await fetch("https://api.datamuse.com/words?rel_trg="+solution_word);
