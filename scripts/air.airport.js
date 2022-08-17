a1c15283add3cff0278c628d7ae72e2976dbcd4e
jdszekeres <69050063+jdszekeres@users.noreply.github.com>
2022-08-16 17:19:09 -0700

    Create air.airport.js

---
+++ scripts/air.airport.js
@@ -0,0 +1,20 @@
+async function get_airport() {
+  var json = await fetch("resources/airports.json");
+  var resp = await json.json();
+  final=[]
+  resp.forEach((airport)=>{
+    if (airport.type==="airport" && airport.name !== null && airport.size !== "small") {
+      final.push(airport)
+    }
+  })
+  return final
+}
+function search(airports,code) {
+  p=null;
+  airports.forEach((airport)=>{
+    if (airport.iata === code.toUpperCase()) {
+      p=airport
+    }
+  })
+  return p
+}
