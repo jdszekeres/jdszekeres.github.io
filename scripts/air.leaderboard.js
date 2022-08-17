57fb8aae50f8c90b60d50411b281c4f132b6e637
jdszekeres <69050063+jdszekeres@users.noreply.github.com>
2022-08-16 17:20:25 -0700

    Create air.leaderboard.js

---
+++ scripts/air.leaderboard.js
@@ -0,0 +1,34 @@
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+const currentGameID = 'airportgame-jdszekeres';
+
+const setData = async (userName, userScore) => {
+  const connect = await fetch(`${baseURL}/${currentGameID}/scores`, {
+    method: 'POST',
+    body: JSON.stringify({ user: userName, score: userScore }),
+    headers: { 'Content-type': 'application/JSON' },
+  });
+  const receivedData = await connect.json();
+  return receivedData;
+};
+
+const getData = async () => {
+  const connect = await fetch(`${baseURL}/${currentGameID}/scores`);
+  const scoreList = await connect.json();
+  return scoreList;
+};
+
+const show = () => {
+  const pointsTable = document.querySelector('#points');
+  pointsTable.innerHTML = '';
+  getData().then((dataList) => {
+    if (!dataList) {
+      return;
+    }
+    const higherFirst = dataList.result.sort((a, b) => b.score - a.score);
+    higherFirst.forEach((data) => {
+      const li = document.createElement('li');
+      li.innerHTML = `<p>${data.user}: ${data.score}</p>`;
+      pointsTable.appendChild(li);
+    });
+  });
+};
