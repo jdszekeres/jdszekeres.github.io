7963a42ac102826b16527e9d57f407af5d5dda80
jdszekeres <69050063+jdszekeres@users.noreply.github.com>
2023-10-12 13:01:07 -0700

    Update air.script.js

--- scripts/air.script.js
+++ scripts/air.script.js
@@ -1,41 +1,24 @@
-var airports;
-var total;
-var guessed=[];
-var code=document.getElementById("code");
-async function start() {
-  airports = await get_airport()
-  total = airports.length;
-  document.getElementById("guess").innerHTML=`you have guessed ${guessed.length}/${total} airports`
-  await show()
-}
-start()
-function add_table(guess) {
-  td=document.createElement("div")
-  td.innerHTML=`${guess.name} (${guess.iata})<img src="https://countryflagsapi.com/png/${guess.iso}" width="32px" height="16px">`
-  document.getElementById("found").appendChild(td)
-}
-function find(event) {
-    if (event.key === 'Enter') {
-      var airport=search(airports,code.value)
-      if (airport !== null) {
-        if (!guessed.includes(airport)) {
-          guessed.push(airport)
-          document.getElementById("guess").innerHTML=`you have guessed ${guessed.length}/${total} airports`
-          add_table(airport)
-          code.value=""
-        } 
-      } else {
-        try {
-        alert(`you already guessed ${airport.iata}`)
-        } catch (e) {
-          alert("bad airport code")
-        }
-      }
-  }
-}
+var airports = [];
+var total = 0;
+var guessed = [];
+(function ()  {
+    fetch("USAirports.json").then(x=>x.json()).then(x=>{airports=x;total = airports.length;document.getElementById("guessed2").innerText = total})
 
-function add_lb() {
-  setData(prompt("what is your name", "anonymous"), guessed.length)
-  setTimeout(()=>{location.reload()},1000)
-  
+}) ()
+document.getElementById("guesser").addEventListener("keypress", (event)=>{if (event.key === "Enter") {check_airport()}});
+
+function check_airport() {
+    value = document.getElementById("guesser").value
+    info = undefined;
+    airports.forEach((airport) => {
+        if (airport.FAA == value.toUpperCase() && !guessed.includes(value.toUpperCase())) {
+            info = airport
+            guessed.push(value.toUpperCase())
+            return;
+        }
+    })
+    document.getElementById("airports").innerHTML += "<p class='airport'>"+info["name"]+" ("+info["FAA"]+")</p>"
+    document.getElementById("guesser").value = ""
+    document.getElementById("guessed1").innerText = guessed.length
+    document.getElementById("guessed2").innerText = total
 }
