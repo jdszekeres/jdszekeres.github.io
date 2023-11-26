d5b15fc585c108e1db8769f3bf01e0b6a98a08ea
jdszekeres <jdszekeres@gmail.com>
2023-11-26 08:38:50 -0800

    compress project images for faster load time

---
+++ minify_images.py
@@ -0,0 +1,18 @@
+import PIL.Image as Image
+import os
+def compress_images(directory, max_width=512):
+    for filename in os.listdir(directory):
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            image = Image.open(os.path.join(directory, filename))
+            width, height = image.size
+
+            if width > max_width:
+                new_height = int(height * max_width / width)
+                image = image.resize((max_width, new_height), Image.ANTIALIAS)
+
+            compressed_filename = os.path.join(directory, filename)
+            image.save(compressed_filename, optimize=True, quality=80)
+
+if __name__ == '__main__':
+    directory = 'project-photos'
+    compress_images(directory)
